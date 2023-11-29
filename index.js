7b904e04ba94ce1bb69c5835d51da29582715da2
caleblaroche14 <69857383+caleblaroche14@users.noreply.github.com>
2023-11-29 07:08:55 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 function isMobileCheck(){
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
     const desktopDivs = document.querySelectorAll(".desktop");
-    var requestmobile = document.getElementsByClassName('mobile')[0];
+    const navbuttons = document.querySelectorAll('.navbutton');
+    const toplogo = document.getElementById("navlogo");
+    const mobiletoplogo = document.getElementById("mobilenavlogo");
+    const requestmobile = document.getElementsByClassName('mobile')[0];
     if (isMobile) {
-    console.log('is mobile');
-    for (var e = 0; e < desktopDivs.length; e++){
-        console.log(desktopDivs[e]);
-        desktopDivs[e].classList.remove("desktop");
-    }
-    requestmobile.style.display = "fixed";
+        console.log('is mobile');
+        for (var e = 0; e < desktopDivs.length; e++){
+            console.log(desktopDivs[e]);
+            desktopDivs[e].classList.remove("desktop");
+        }
+        for (var e = 0; e < navbuttons.length; e++){
+            console.log(navbuttons[e]);
+            navbuttons[e].classList.add("mobile");
+        }
+        if (requestmobile){
+            requestmobile.style.display = "fixed";
+        }
+        if (mobiletoplogo){
+            mobiletoplogo.style.display = "flex";
+        }
+        toplogo.style.display = "none";
+        console.log(toplogo);
+
     }else{
         console.log('not mobile');
-        requestmobile.style.display = "none";
+        if (requestmobile){
+            requestmobile.style.display = "none";
+        }
+
     }
 }
 isMobileCheck();
